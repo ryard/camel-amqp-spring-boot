972d8a25a3c657253417b6c5b15819bb438cb9ba
Jia Jun Ng <ngjiajun13@gmail.com>
2020-03-10 11:07:28 +0800

    updated camel

--- src/main/java/com/redhat/demo/Application.java
+++ src/main/java/com/redhat/demo/Application.java
@@ -8,10 +8,10 @@ import org.springframework.jms.annotation.EnableJms;
 @SpringBootApplication
 @EnableJms
 @ImportResource({"classpath:spring/camel-context.xml"})
-public class SpringBootAmqpConsumerApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAmqpConsumerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
